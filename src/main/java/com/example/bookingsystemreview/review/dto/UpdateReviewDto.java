d009f3075eee925f9f4ced07ebdc51e3ae332047
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-08-22 00:59:12 +0200

    Created dto for updating DB

---
+++ src/main/java/com/example/bookingsystemreview/review/dto/UpdateReviewDto.java
@@ -0,0 +1,15 @@
+package com.example.bookingsystemreview.review.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateReviewDto(
+        @NotNull
+        String reviewContent,
+        @NotNull
+        @Min(1)
+        @Max(5)
+        Integer reviewScore
+) {
+}
