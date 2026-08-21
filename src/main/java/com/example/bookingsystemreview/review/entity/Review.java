7fdbba473ce586ac02aa32463438eab8c392131d
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-08-22 00:51:37 +0200

    Created the entity Review

---
+++ src/main/java/com/example/bookingsystemreview/review/entity/Review.java
@@ -0,0 +1,93 @@
+package com.example.bookingsystemreview.review.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long roomId;
+
+    @Column(nullable = false)
+    private String reviewContent;
+
+    @Column(nullable = false)
+    private Integer reviewScore;
+
+    @CreationTimestamp
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    private LocalDateTime creationDate;
+
+    @UpdateTimestamp
+    @Column(name = "update_date", nullable = false)
+    private LocalDateTime updateDate;
+
+    public Review() {}
+
+    public Review(Long userId,
+                  Long roomId,
+                  String reviewContent,
+                  Integer reviewScore) {
+        this.userId = userId;
+        this.roomId = roomId;
+        this.reviewContent = reviewContent;
+        this.reviewScore = reviewScore;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getReviewContent() {
+        return reviewContent;
+    }
+    public void setReviewContent(String reviewContent) {
+        this.reviewContent = reviewContent;
+    }
+
+    public Integer getReviewScore() {
+        return reviewScore;
+    }
+    public void setReviewScore(Integer reviewScore) {
+        this.reviewScore = reviewScore;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+    public void setCreationDate(LocalDateTime reviewDate) {
+        this.creationDate = reviewDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+}
