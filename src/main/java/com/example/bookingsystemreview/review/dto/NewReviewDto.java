fb1e65448001197c5d27366216c9642e65c41c4d
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-08-22 00:59:29 +0200

    Created dto for creating a new review

---
+++ src/main/java/com/example/bookingsystemreview/review/dto/NewReviewDto.java
@@ -0,0 +1,18 @@
+package com.example.bookingsystemreview.review.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record NewReviewDto(
+        @NotNull
+        Long userId,
+        @NotNull
+        Long roomId,
+        @NotNull
+        String reviewContent,
+        @NotNull
+        @Min(1)
+        @Max(5)
+        Integer reviewScore) {
+}
