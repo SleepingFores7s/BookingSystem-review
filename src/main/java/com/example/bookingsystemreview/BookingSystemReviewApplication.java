4c3e1fb4f1841279a1ff3541878c12656145bb51
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-08-21 16:35:09 +0200

    Initial commit

---
+++ src/main/java/com/example/bookingsystemreview/BookingSystemReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingsystemreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSystemReviewApplication.class, args);
+    }
+
+}
