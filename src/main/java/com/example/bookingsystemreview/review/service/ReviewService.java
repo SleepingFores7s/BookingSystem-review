90dc5387bf34a75da7f2fba0cf6282de9c4f2d42
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-08-24 10:29:10 +0200

    Expanded methods to include (Update by id, Delete by id)

--- src/main/java/com/example/bookingsystemreview/review/service/ReviewService.java
+++ src/main/java/com/example/bookingsystemreview/review/service/ReviewService.java
@@ -2,9 +2,11 @@ package com.example.bookingsystemreview.review.service;
 
 import com.example.bookingsystemreview.review.dto.NewReviewDto;
 import com.example.bookingsystemreview.review.dto.ReviewResponseDto;
+import com.example.bookingsystemreview.review.dto.UpdateReviewDto;
 import com.example.bookingsystemreview.review.entity.Review;
 import com.example.bookingsystemreview.review.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -61,6 +63,41 @@ public class ReviewService {
 
         }catch (Exception e){
             return null;
+        }
+    }
+
+    public ReviewResponseDto updateReviewById(Long id, @RequestBody UpdateReviewDto dto) {
+        try {
+            Review review = reviewRepository.getReviewById(id);
+            if (review == null) {
+                return null;
+            }
+            review.setReviewContent(dto.reviewContent());
+            review.setReviewScore(dto.reviewScore());
+
+            reviewRepository.save(review);
+
+            return new ReviewResponseDto(
+                    review.getId(),
+                    review.getReviewContent(),
+                    review.getReviewScore(),
+                    review.getCreationDate(),
+                    review.getUpdateDate()
+            );
+
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public boolean deleteReviewById(Long id) {
+        try{
+
+            reviewRepository.deleteById(id);
+            return true;
+
+        }catch (Exception e){
+            return false;
         }
     }
 
