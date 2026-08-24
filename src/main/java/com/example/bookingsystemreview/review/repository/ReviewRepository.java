54348618aacf840f05b9074191753f10e7fcdbf1
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-08-24 17:30:11 +0200

    Removed getAllReviews as it was redundant, instead findAll() will be used

--- src/main/java/com/example/bookingsystemreview/review/repository/ReviewRepository.java
+++ src/main/java/com/example/bookingsystemreview/review/repository/ReviewRepository.java
@@ -1,13 +1,9 @@
 package com.example.bookingsystemreview.review.repository;
 
-import com.example.bookingsystemreview.review.dto.ReviewResponseDto;
 import com.example.bookingsystemreview.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> getAllReviews();
 
     Review getReviewById(Long id);
 }
