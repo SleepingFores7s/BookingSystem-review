e87a7d77c2843a61eff63fb76e47126e68500016
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-08-31 02:04:38 +0200

    Implemented JWT-checking

---
+++ src/main/java/com/example/bookingsystemreview/security/jwt/filter/JwtFilter.java
@@ -0,0 +1,41 @@
+package com.example.bookingsystemreview.security.jwt.filter;
+
+import com.example.bookingsystemreview.security.jwt.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            if (jwtService.isTokenValid(token)) {
+                var authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+                var user = jwtService.extractUsername(token);
+                var auth = new UsernamePasswordAuthenticationToken(user, null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+}
