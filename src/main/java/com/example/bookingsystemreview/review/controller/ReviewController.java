09d4a76b60827bb80f7d766b4c7cb37d8c9dd6f2
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-08-31 23:09:48 +0200

    Added comments for easier method finding & made method logic MUCH smaller.

--- src/main/java/com/example/bookingsystemreview/review/controller/ReviewController.java
+++ src/main/java/com/example/bookingsystemreview/review/controller/ReviewController.java
@@ -3,14 +3,10 @@ package com.example.bookingsystemreview.review.controller;
 import com.example.bookingsystemreview.review.dto.NewReviewDto;
 import com.example.bookingsystemreview.review.dto.ReviewResponseDto;
 import com.example.bookingsystemreview.review.dto.UpdateReviewDto;
-import com.example.bookingsystemreview.review.entity.Review;
 import com.example.bookingsystemreview.review.service.ReviewService;
-import org.apache.tomcat.util.http.parser.Authorization;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,92 +14,61 @@ import java.util.List;
 @RestController
 public class ReviewController {
 
-    /*TODO C.R.U.D
-    * 1. OK Get all reviews (admin)
-    * 2. OK Get all reviews from 1 user (admin/user)
-    * 3. OK Write a review to DB (user)
-    * 4. OK Change a users review (user) (indicate that its been changed both in DB and to user)
-    * 5. OK Remove a users review (admin (any)/user (their own)))
-    *
-    * */
-
     private final ReviewService reviewService;
 
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
     }
 
+    //TEST
     @GetMapping("/test")
     public String testMessage() {
         return "test";
     }
 
+    //GET ALL REVIEWS
     @GetMapping("/reviews")
-    public ResponseEntity<?> getAllReviews() {
+    public ResponseEntity<List<ReviewResponseDto>> getAllReviews() {
 
-        List<Review> reviews = reviewService.getAllReviews();
+        List<ReviewResponseDto> reviews = reviewService.getAllReviews();
 
         if(reviews.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
-        return ResponseEntity.ok(reviewService.getAllReviews());
+        return ResponseEntity.ok(reviews);
 
     }
 
-    @GetMapping("/reviews/user")
-    public ResponseEntity<?> getReviewsByUser(@AuthenticationPrincipal Jwt jwt) {
-
-        Long userId = jwt.getClaim("userId");
-
-        List<ReviewResponseDto> reviews = reviewService.getReviewsByUserId(userId);
-        if(reviews.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        }
-            return ResponseEntity.ok(reviews);
+    //GET USER REVIEWS
+    @GetMapping("/reviews/user/{userId}")
+    public ResponseEntity<List<ReviewResponseDto>> getReviewsByUserId(@PathVariable Long userId) {
+        return ResponseEntity.ok(reviewService.getReviewsByUserId(userId));
     }
 
+    //CREATE REVIEW
     @PostMapping("/reviews")
-    public ResponseEntity<?> createReview(@RequestBody NewReviewDto newReview) {
-
-        ReviewResponseDto dto = reviewService.createNewReview(newReview);
-        if(dto == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-        return ResponseEntity.ok(dto);
-
+    public ResponseEntity<ReviewResponseDto> createNewReview(@Valid @RequestBody NewReviewDto newReview) {
+            ReviewResponseDto created = reviewService.createNewReview(newReview);
+            return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
+    //GET REVIEW USING ID
     @GetMapping("/reviews/{id}")
-    public ResponseEntity<?> getReviewById(@PathVariable Long id) {
-
-        try{
-
+    public ResponseEntity<ReviewResponseDto> getReviewById(@PathVariable Long id) {
             return ResponseEntity.ok(reviewService.getReviewById(id));
-
-        }catch(Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-
     }
 
+    //UPDATE REVIEW
     @PutMapping("/reviews/{id}")
-    public ResponseEntity<?> updateReviewById(@PathVariable Long id, @RequestBody UpdateReviewDto updateReview) {
-
-        try {
+    public ResponseEntity<ReviewResponseDto> updateReviewById(@PathVariable Long id, @Valid @RequestBody UpdateReviewDto updateReview) {
             return ResponseEntity.ok(reviewService.updateReviewById(id, updateReview));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
     }
 
+    //DELETE REVIEW
     @DeleteMapping("/reviews/{id}")
-    public ResponseEntity<?> deleteReviewById(@PathVariable Long id) {
-        try{
-            boolean deleted = reviewService.deleteReviewById(id);
-            return ResponseEntity.ok().body(deleted);
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public ResponseEntity<Void> deleteReviewById(@PathVariable Long id) {
+            reviewService.deleteReviewById(id);
+            return ResponseEntity.noContent().build();
     }
 
 }
