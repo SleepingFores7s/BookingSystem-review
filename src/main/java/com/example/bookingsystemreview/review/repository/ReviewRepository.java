2c0dd84e60ddac720cebb709bfae9c8b908ddf14
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-09-07 00:51:55 +0200

    Add: -added method to get room reviews using room id

--- src/main/java/com/example/bookingsystemreview/review/repository/ReviewRepository.java
+++ src/main/java/com/example/bookingsystemreview/review/repository/ReviewRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<ReviewResponseDto> findAllByUserId(Long userId);
+    List<ReviewResponseDto> findAllByRoomId(Long roomId);
 }
