f50745b938cbacfe0e4b84cea81ae97db3394333
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-09-08 04:51:18 +0200

    Refactored to be more clear what variable it takes

--- src/main/java/com/example/bookingsystemreview/review/dto/ReviewResponseDto.java
+++ src/main/java/com/example/bookingsystemreview/review/dto/ReviewResponseDto.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public record ReviewResponseDto(
         Long id,
         Long userId,
-        Long roomId,
+        Long roomId, //todo refactor so it says room number, not id.
         String reviewContent,
         Integer reviewScore,
         LocalDateTime creationDate,
