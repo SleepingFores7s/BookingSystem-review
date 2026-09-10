129dddf39db5da47a2dae1a1a9c22c70654cc6d3
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-09-10 02:31:42 +0200

    Changed from roomId to roomNumber for better clarity.

--- src/main/java/com/example/bookingsystemreview/review/dto/ReviewResponseDto.java
+++ src/main/java/com/example/bookingsystemreview/review/dto/ReviewResponseDto.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public record ReviewResponseDto(
         Long id,
         Long userId,
-        Long roomId, //todo refactor so it says room number, not id.
+        Integer roomNumber,
         String reviewContent,
         Integer reviewScore,
         LocalDateTime creationDate,
