0cb0b08cda2add3dcf8b4d8cc128d5404ae57a35
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-09-17 01:18:17 +0200

    Added a way for actions to push up the new image, then access my server and pull the new image.

--- src/main/java/com/example/bookingsystemreview/BookingSystemReviewApplication.java
+++ src/main/java/com/example/bookingsystemreview/BookingSystemReviewApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookingSystemReviewApplication {
 
     public static void main(String[] args) {
+        System.out.println("Test for docker upload and server pull");
         SpringApplication.run(BookingSystemReviewApplication.class, args);
     }
 
