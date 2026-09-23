92de2d5385101864c5ee69552fa56e992df6e981
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-09-23 08:49:25 +0200

    added .env to gitignore

--- src/main/java/com/example/bookingsystemreview/BookingSystemReviewApplication.java
+++ src/main/java/com/example/bookingsystemreview/BookingSystemReviewApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookingSystemReviewApplication {
 
+    //test commit comment
+
     public static void main(String[] args) {
         SpringApplication.run(BookingSystemReviewApplication.class, args);
     }
